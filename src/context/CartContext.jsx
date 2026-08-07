4148f6f565ea6123bc89d53bb2bc2ac9c16ccf69
Abu Bakar <abubakar@example.com>
2026-08-07 12:40:49 -0700

    Fix Context export errors for Vite build

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const CartContext = createContext();
+export const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
